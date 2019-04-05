290b9a7ba34b1a88462f170b7708d0196ef70499
Josh Kruse <jek248@nau.edu>
2019-04-05 16:15:11 -0700

    Added Comments to classes, Checked unit tests

--- app/src/main/java/com/example/food2go/OrderItems.java
+++ app/src/main/java/com/example/food2go/OrderItems.java
@@ -4,29 +4,35 @@ import java.util.ArrayList;
 
 public class OrderItems
 {
+    //initialize variables
     ArrayList<MenuItem> order = new ArrayList<MenuItem>();
 
+    //adds MenuItem object to order with parameters
     public void addToOrder( double price, String item_name, String feedback )
     {
         MenuItem new_item = new MenuItem( price, item_name, feedback );
         this.order.add( new_item );
     }
 
+    //adds MenuItem object from parameter to order
     public void addToOrder( MenuItem item )
     {
         this.order.add( item );
     }
 
+    //removes MenuItem object from order
     public void removeFromOrder( MenuItem item )
     {
         this.order.remove( item );
     }
 
+    //Calculates the quantity or Order
     public int getQuantity()
     {
         return order.size();
     }
 
+    //Calculates the total of an order
     public int getTotal()
     {
         int index;
