98dfedff14fbde01287d4c26842f6af2eef77d4f
Josh Kruse <jek248@nau.edu>
2019-04-08 10:40:37 -0700

    Fixed a few errors still needs testing

--- app/src/main/java/com/example/food2go/SignupActivity.java
+++ app/src/main/java/com/example/food2go/SignupActivity.java
@@ -4,29 +4,31 @@ import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import butterknife.ButterKnife;
-import butterknife.InjectView;
-
-public class SignupActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity
+{
     private static final String TAG = "SignupActivity";
 
-    @InjectView(R.id.input_name) EditText _nameText;
-    @InjectView(R.id.input_email) EditText _emailText;
-    @InjectView(R.id.input_password) EditText _passwordText;
-    @InjectView(R.id.btn_signup) Button _signupButton;
-    @InjectView(R.id.link_login) TextView _loginLink;
+    EditText _nameText = findViewById(R.id.input_name);
+    EditText _emailText = findViewById(R.id.input_email);
+    EditText _usernameText = findViewById(R.id.input_username);
+    EditText _passwordText = findViewById(R.id.input_password);
+    EditText _phoneText = findViewById(R.id.input_phone);
+    EditText _addressText = findViewById(R.id.input_address);
+    Button _signupButton = findViewById(R.id.btn_signup);
+    TextView _loginLink = findViewById(R.id.link_login);
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-        ButterKnife.inject(this);
 
         _signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,7 +46,8 @@ public class SignupActivity extends AppCompatActivity {
         });
     }
 
-    public void signup() {
+    public void signup()
+    {
         Log.d(TAG, "Signup");
 
         if (!validate()) {
@@ -55,7 +58,7 @@ public class SignupActivity extends AppCompatActivity {
         _signupButton.setEnabled(false);
 
         final ProgressDialog progressDialog = new ProgressDialog(SignupActivity.this,
-                R.style.AppTheme_Dark_Dialog);
+                R.style.AppTheme);
         progressDialog.setIndeterminate(true);
         progressDialog.setMessage("Creating Account...");
         progressDialog.show();
@@ -79,44 +82,83 @@ public class SignupActivity extends AppCompatActivity {
     }
 
 
-    public void onSignupSuccess() {
+    public void onSignupSuccess()
+    {
         _signupButton.setEnabled(true);
         setResult(RESULT_OK, null);
         finish();
     }
 
-    public void onSignupFailed() {
+    public void onSignupFailed()
+    {
         Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
 
         _signupButton.setEnabled(true);
     }
 
-    public boolean validate() {
+    public boolean validate()
+    {
         boolean valid = true;
 
         String name = _nameText.getText().toString();
         String email = _emailText.getText().toString();
+        String username = _usernameText.getText().toString();
         String password = _passwordText.getText().toString();
+        String phone = _phoneText.getText().toString();
+        String address = _addressText.getText().toString();
 
-        if (name.isEmpty() || name.length() < 3) {
+        //Name
+        if (name.isEmpty() || name.length() < 3)
+        {
             _nameText.setError("at least 3 characters");
             valid = false;
         } else {
             _nameText.setError(null);
         }
 
-        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+        //Email
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        {
             _emailText.setError("enter a valid email address");
             valid = false;
         } else {
             _emailText.setError(null);
         }
 
-        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+        //Username
+        if (username.isEmpty())
+        {
+            _usernameText.setError("enter a valid username");
+            valid = false;
+        } else {
+            _usernameText.setError(null);
+        }
+
+        //Password
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10)
+        {
             _passwordText.setError("between 4 and 10 alphanumeric characters");
             valid = false;
         } else {
             _passwordText.setError(null);
+        }
+
+        //Phone Number
+        if (phone.isEmpty() || !Patterns.PHONE.matcher(phone).matches())
+        {
+            _phoneText.setError("enter a valid phone number");
+            valid = false;
+        } else {
+            _phoneText.setError(null);
+        }
+
+        //Address
+        if (address.isEmpty())
+        {
+            _addressText.setError("enter a valid address");
+            valid = false;
+        } else {
+            _addressText.setError(null);
         }
 
         return valid;
