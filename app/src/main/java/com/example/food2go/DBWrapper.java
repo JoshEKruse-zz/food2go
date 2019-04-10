5f82f77a5fd683fc22fa417854a65efe48bdf9fa
Josh Kruse <jek248@nau.edu>
2019-04-09 19:33:55 -0700

    Revert "Working on test DB"
    This reverts commit 875d4a68c1998bd8d7f6e9d143bf1a6b18057243.

---
+++ app/src/main/java/com/example/food2go/DBWrapper.java
@@ -0,0 +1,44 @@
+package com.example.food2go;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.*;
+import android.util.*;
+
+public class DBWrapper extends SQLiteOpenHelper {
+    // https://www.youtube.com/watch?v=aQAIMY-HzL8
+    //https://www.tutorialspoint.com/android/android_sqlite_database.htm
+    private static final String TAG = "DatabaseHelper";
+
+    private static final String TABLE_NAME = "test_table";
+    private static final String COL1 = "ID";
+    private static final String COL2 = "name";
+
+    public DatabaseHelper(Context context)
+    {
+        super(context, TABLE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db)
+    {
+      String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, "
+              + COL2 +" TEXT)";
+      db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int j)
+    {
+        db.execSQL("DROP IF TABLE EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean addData(String item)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+    }
+
+}
