62fdee6b63e2775e8917dd625d30c5afe1b64437
Jessica <jls924@nau.edu>
2019-04-09 18:33:31 -0700

    Revert "Working on test DB"
    This reverts commit 875d4a68c1998bd8d7f6e9d143bf1a6b18057243.

--- app/src/main/java/com/example/food2go/DBWrapper.java
+++ app/src/main/java/com/example/food2go/DBWrapper.java
@@ -1,44 +1,24 @@
 package com.example.food2go;
 
-import android.content.ContentValues;
-import android.content.Context;
-import android.database.DatabaseErrorHandler;
-import android.database.sqlite.*;
-import android.util.*;
+import java.sql.*;
+import java.util.Random;
 
-public class DBWrapper extends SQLiteOpenHelper {
-    // https://www.youtube.com/watch?v=aQAIMY-HzL8
-    //https://www.tutorialspoint.com/android/android_sqlite_database.htm
-    private static final String TAG = "DatabaseHelper";
+public class DBWrapper {
 
-    private static final String TABLE_NAME = "test_table";
-    private static final String COL1 = "ID";
-    private static final String COL2 = "name";
+    private static Connection connect;
 
-    public DatabaseHelper(Context context)
+    public DBWrapper()
     {
-        super(context, TABLE_NAME, null, 1);
+
+        try {
+            // get driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // establish connection
+            connect = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/foodToGo", "root", "enterPassword");
+        }
+        catch (Exception excpt){System.out.println("Connection failed.  Error: " + excpt);}
     }
 
-    @Override
-    public void onCreate(SQLiteDatabase db)
-    {
-      String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, "
-              + COL2 +" TEXT)";
-      db.execSQL(createTable);
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int i, int j)
-    {
-        db.execSQL("DROP IF TABLE EXISTS " + TABLE_NAME);
-        onCreate(db);
-    }
-
-    public boolean addData(String item)
-    {
-        SQLiteDatabase db = this.getWritableDatabase();
-
-    }
 
 }
