b34f66bec51829ea1d41965b5df6f6dc0ee98c97
Jessica <jls924@nau.edu>
2019-04-24 13:31:11 -0700

    Menu XML has two images
    Working on what view type would work best

--- app/src/main/java/com/example/food2go/MenuActivity.java
+++ app/src/main/java/com/example/food2go/MenuActivity.java
@@ -1,4 +1,31 @@
 package com.example.food2go;
 
-public class MenuActivity {
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MenuActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_main);
+    }
 }
